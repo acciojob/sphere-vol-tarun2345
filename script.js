42e35e366a81c6aa887b0507e1634004e7f4cf50
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-04-19 19:30:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function volume_sphere() {
     //Write your code here
-	let volume = (4/3) * Math.PI * Math.pow(radius.value, 3);
-  return volume;
+	volume.value = (4/3) * Math.PI * Math.pow(radius.value, 3);
 } 
 
 let radius = document.getElementById('radius');
+let volume =  document.getElementById('volume');
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
